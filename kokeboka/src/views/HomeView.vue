c32029ad12ef19ae497c85beec701b50b73ad16f
David <davidmariusk@gmail.com>
2023-08-15 14:54:38 +0200

    install tailwindcss

--- kokeboka/src/views/HomeView.vue
+++ kokeboka/src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<main-layout></main-layout>
 </template>
+
+<script setup lang="ts">
+import MainLayout from '../layouts/MainLayout.vue';
+
+</script>
